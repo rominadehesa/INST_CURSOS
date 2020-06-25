0450e2b2fa5d93c1072dec33cac74e2bbbbc8e0c
Romina Dehesa <rominadehesa@gmail.com>
2020-06-24 21:34:21 -0300

    funcionan los endpoints hechos

--- api/api.view.php
+++ api/api.view.php
@@ -1,11 +1,19 @@
 <?php
 
 class APIview{
-    public function response($array) {//devuelve un arreglo en formatp Json
-        header("Content-Type: application/json");
-        echo json_encode($array);
+    public function response($data, $status) {
+        header("Content-Type: application/json"); //avisa que devuelve json
+        header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
+        echo json_encode($data);
     }
 
-
+    private function _requestStatus($code){
+        $status = array(
+          200 => "OK",
+          404 => "Not found",
+          500 => "Internal Server Error"
+        );
+        return (isset($status[$code]))? $status[$code] : $status[500];
+    }
 }
 
--- router-api.php
+++ router-api.php
@@ -2,15 +2,18 @@
 
 require_once 'libs/Router/Router.php';
 require_once 'api/areas-api.controller.php';
+//require_once 'api/comments-api.controller.php';
 
 //creo el ruteador
 $router= new Router();
 
 //creo la tabla de ruteo
-$router->addRoute('areas', 'GET', 'AreasApiController', 'getAreas');
+$router->addRoute('areas', 'GET', 'AreasApiController', 'getAreas'); 
 $router->addRoute('areas/:ID', 'GET', 'AreasApiController', 'getArea');
-$router->addRoute('areas/:ID', 'DELETE', 'AreasApiController', 'deleteArea');
 
+//$router->addRoute('comments', 'GET', 'CommentsApiController', 'getComments');
+//$router->addRoute('comments/:ID', 'GET', 'CommentsApiController', 'getComment');
+//$router->addRoute('comments/:ID', 'DELETE', 'CommentsApiController', 'deleteComment');
 
 // rutea
 $router->route($_REQUEST['resource'], $_SERVER['REQUEST_METHOD']);
